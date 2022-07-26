27562162e267d413f42ad0faade48a279073acec
Nihal Aghazada <nihal@nevercode.io>
2022-07-26 10:25:13 +0300

    moving path

---
+++ scripts/delete.sh
@@ -0,0 +1,1 @@
+curl -XDELETE -H 'X-Auth-Token: UjW3noRBwGnyBlOOfHhc8suwuNZmhT4XP7SaeXV-Z7c' -H "Content-type: application/json" 'https://api.codemagic.io/apps/62df95ab77328efbfac6b305/variables/BUNDLE_ID'
